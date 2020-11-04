7bd9b3a8b6b2803dfb5defcb3a3be324d5dfa0b0
AnnemarieFromm <73169443+AnnemarieFromm@users.noreply.github.com>
2020-11-04 07:30:08 +0100

    er

--- Versuch 1/Assets/Skript/ERObjekt.cs
+++ Versuch 1/Assets/Skript/ERObjekt.cs
@@ -5,37 +5,68 @@ using UnityEngine;
 
 public class ERObjekt : MonoBehaviour
 {
-    private bool selected;
+    private float startPosX;
+    private float startPosY;
+    private bool selected = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-
-
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        if (selected == true)
+        if (selected)
         {
-            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(cursorPos.x, cursorPos.y, 0);
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
+            gameObject.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0);
         }
     }
 
-    void OnMouseOver()
+    private void OnMouseDown()
     {
-        Debug.Log("Test!");
         if (Input.GetMouseButtonDown(0))
         {
             selected = true;
-
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            selected = false;
         }
     }
+
+    private void OnMouseUp()
+    {
+        selected = false;
+    }
+
 }
+
+/*private bool selected;
+
+// Start is called before the first frame update
+void Start()
+{
+
+
+}
+// Update is called once per frame
+void Update()
+{
+    Debug.Log("Start!");
+    if (selected == true)
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(cursorPos.x, cursorPos.y, 0);
+
+    }
+}
+
+void OnMouseOver()
+{
+    Debug.Log("Test!");
+    if (Input.GetMouseButtonDown(0))
+    {
+        selected = true;
+
+    }
+
+    if (Input.GetMouseButtonUp(0))
+    {
+        selected = false;
+    }
+}*/
