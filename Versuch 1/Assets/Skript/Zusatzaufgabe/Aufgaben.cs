b9344528eea7b2620a1b4e6a40b58145c219c44a
UndiMeSMarcel <68282545+UndiMeSMarcel@users.noreply.github.com>
2021-04-06 14:55:13 +0200

    Bilder eingefügt
    Habe die Aufgaben schonmal eingefügt und die Ergebnisse eingetragem

--- Versuch 1/Assets/Skript/Zusatzaufgabe/Aufgaben.cs
+++ Versuch 1/Assets/Skript/Zusatzaufgabe/Aufgaben.cs
@@ -14,11 +14,39 @@ public class Aufgaben : MonoBehaviour
     public toogleEingabe toogle;
     public GameObject exitKnopf;
     public GameObject RichitgFalschAnzeige;
+    public GameObject fenster;
+    public GameObject checkButton;
 
     public void Start()
     {
         correct = new string[aufgabenListe.Length];
-        correct[0] = "B";
+        correct[0] = "C";
+        correct[1] = "C";
+        correct[2] = "C";
+        correct[3] = "A";
+        correct[4] = "B";
+        correct[5] = "A";
+        correct[6] = "C";
+        correct[7] = "D";
+        correct[8] = "D";
+        correct[9] = "C";
+        correct[10] = "B";
+        correct[11] = "D";
+        correct[12] = "C";
+        correct[13] = "D";
+        correct[14] = "B";
+        correct[15] = "C";
+        correct[16] = "A";
+        correct[17] = "B";
+        correct[18] = "C";
+        correct[19] = "B";
+        correct[20] = "D";
+        correct[21] = "A";
+        correct[22] = "D";
+        correct[23] = "C";
+        correct[24] = "D";
+        correct[25] = "D";
+        
         //correct={"B",....}
         level = new int[aufgabenListe.Length]; //?
     }
@@ -32,11 +60,20 @@ public class Aufgaben : MonoBehaviour
             welcheAufgabe++;
             geldGeben();
             exitKnopf.SetActive(true);
+            checkButton.SetActive(false);
+    
+            Invoke("closeFenster", 2);
         }
         else
         {
-            Utilitys.TextInTMP(RichitgFalschAnzeige, "Falsch !");
-            Invoke("clearAnzeige", 8);
+            string ausgabe = "Falsch! Richtig ist " + correct[welcheAufgabe];
+            Utilitys.TextInTMP(RichitgFalschAnzeige, ausgabe);
+            //Invoke("clearAnzeige", 4);
+            exitKnopf.SetActive(true);
+            checkButton.SetActive(false);
+
+            welcheAufgabe++;
+            Invoke("closeFenster", 8);
         }
     }
 
@@ -45,6 +82,12 @@ public class Aufgaben : MonoBehaviour
         Utilitys.TextInTMP(RichitgFalschAnzeige, " ");
     }
 
+    public void closeFenster()
+    {
+        fenster.SetActive(false);
+    }
+
+
     public void openAufgabe()
     {
         toogle.init();
@@ -52,6 +95,7 @@ public class Aufgaben : MonoBehaviour
         gameObject.GetComponent<Image>().sprite = aufgabenListe[welcheAufgabe];
         exitKnopf.SetActive(false);
         clearAnzeige();
+        checkButton.SetActive(true);
         
     }
 
