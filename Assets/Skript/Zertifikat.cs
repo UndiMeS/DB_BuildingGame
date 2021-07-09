bcf6d5fd1793c37cdfce46a4aa0d99559f06ba75
UndiMeS <67786979+UndiMeS@users.noreply.github.com>
2021-07-09 10:34:08 +0200

    pdf image fix, change to .Net 4.x in setting und System.Draw.dll importiert

--- Assets/Skript/Zertifikat.cs
+++ Assets/Skript/Zertifikat.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using sharpPDF;
 
@@ -7,6 +8,7 @@ using sharpPDF;
 
 public class Zertifikat : MonoBehaviour
 {
+    //public Image myImage;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +26,20 @@ public class Zertifikat : MonoBehaviour
         pdfDocument myDoc = new pdfDocument("Missionsbestätigung","UndiMeS");
         pdfPage myPage = myDoc.addPage();
         myPage.addText("Mein Geld: "+Testing.geld.ToString(), 0, 0, myDoc.getFontReference("Helvetica"), 20);
-        //myDoc.addImageReference(Application.dataPath + @"\SaveState\ERDScreenshot.png","Bild");
-        //myPage.addImage(myDoc.getImageReference("Bild"),400,750);
+        myDoc.addImageReference(Application.dataPath + @"\SaveState\ERDScreenshot.png","Bild");
+        myPage.addImage(myDoc.getImageReference("Bild"),400,750);
+
+        //byte[] imageData = System.IO.File.ReadAllBytes("D/forschungsstation_2.PNG");
+
+        //Image myImage = Image.FromFile(@"forschungsstation_2.PNG");
+        //pdfPage myPage = myDoc.addPage(myImage.Height, myImage.Width);        
+        //myPage.addImage(System.IO.File.ReadAllBytes(imageData), 0, 0, 400, 400);
+        //myPage.addImage("D/forschungsstation_2.PNG", 0, 0, 400, 400);
+
+        //myDoc.addImageReference("D/forschungsstation_2.PNG", "Image");
+
+        //System.Drawing.Image img = System.Drawing.Image.FromFile(imagePath);
+
 
         myDoc.createPDF( Application.dataPath + @"\PDF\missionsbestätigung.pdf");
         myPage = null;
