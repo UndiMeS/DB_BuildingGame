875f91c022b06b7e8c09c7a3c80a6ca8c2afc2e7
UndiMeSMarcel <68282545+UndiMeSMarcel@users.noreply.github.com>
2021-07-09 11:53:55 +0200

    Positionen und Hintergrund angepasst

--- Assets/Skript/Zertifikat.cs
+++ Assets/Skript/Zertifikat.cs
@@ -35,10 +35,10 @@ public class Zertifikat : MonoBehaviour
         myPage.addImage(myDoc.getImageReference("Hintergrund"),0 ,0);
         //Spiel-Screenshot
         myDoc.addImageReference(Application.dataPath + @"\Zertifikatsbilder\Spiel.png","Spiel");
-        myPage.addImage(myDoc.getImageReference("Spiel"),239,1691, 1044, 1878);
+        myPage.addImage(myDoc.getImageReference("Spiel"),241,1692, 1044, 1878);
         //ER-Screenshot
         myDoc.addImageReference(Application.dataPath + @"\Zertifikatsbilder\ERD.png","ERD");
-        myPage.addImage(myDoc.getImageReference("ERD"), 239,476, 1044, 1878);
+        myPage.addImage(myDoc.getImageReference("ERD"), 241,478, 1044, 1878);
 
         //Schriftgröße
         int schriftSize = 50;
@@ -48,7 +48,7 @@ public class Zertifikat : MonoBehaviour
         myDoc.addTrueTypeFont(Application.dataPath + @"\Font\FallingSky-JKwK.ttf", "FallingSky");
         
         //Name und Level
-        myPage.addText("Maxi Mustermensch", 513, 2945, myDoc.getFontReference("AstroSpace"), schriftSize);
+        myPage.addText(Testing.menschen[0].name, 702, 2945, myDoc.getFontReference("AstroSpace"), 40);
         myPage.addText(Story.level.ToString(), 2109, 2946, myDoc.getFontReference("AstroSpace"), schriftSize);
         
         //Siedlungsdaten
