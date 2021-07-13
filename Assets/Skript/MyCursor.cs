3d3836e50e16784bc5e96b793dcd2d0aa3ae3531
UndiMeSMarcel <68282545+UndiMeSMarcel@users.noreply.github.com>
2021-07-13 11:04:16 +0200

    Cursor

--- Assets/Skript/MyCursor.cs
+++ Assets/Skript/MyCursor.cs
@@ -9,12 +9,12 @@ public class MyCursor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.SetCursor(cursorSpiel, Vector2.zero, CursorMode.ForceSoftware);
+        
     }   
 
     // Update is called once per frame
     void Update()
     {
-
+        Cursor.SetCursor(cursorSpiel, Vector2.zero, CursorMode.ForceSoftware);
     }
 }
